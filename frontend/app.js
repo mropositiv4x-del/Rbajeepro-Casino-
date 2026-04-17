9f911009b5d78d5b9b97d33a22d64c8f4aa50dd8
mropositiv4x-del <mropositiv4x@gmail.com>
2026-04-18 03:36:28 +0600

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,13 @@
+let balance = 1000;
 
+function play() {
+  fetch("https://YOUR_BACKEND_URL/play", {
+    method: "POST"
+  })
+  .then(res => res.json())
+  .then(data => {
+    balance += data.win;
+    document.getElementById("balance").innerText = balance;
+    document.getElementById("result").innerText = data.message;
+  });
+}
